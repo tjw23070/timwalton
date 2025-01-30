7048eedc3a6b07eae3bd8cbb0940ce8d0650b63d
tjw23070 <168512762+tjw23070@users.noreply.github.com>
2025-01-30 17:18:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -15,3 +15,15 @@ function checkLogin(username, password) {
 function isFileSizeValid(file) {
     return file.size / (1024 * 1024) <= config.maxFileSizeMB;
 }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCfkxWYFn5exq5fhdePGKvymsLZevoqHo",
+  authDomain: "storage-13e02.firebaseapp.com",
+  projectId: "storage-13e02",
+  storageBucket: "storage-13e02.firebasestorage.app",
+  messagingSenderId: "501311603008",
+  appId: "1:501311603008:web:e17953cf00537751743aa5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
